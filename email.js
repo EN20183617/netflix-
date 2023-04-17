082d62916e14b047157c7fac2687c1095d019462
EN20183617 <90601261+EN20183617@users.noreply.github.com>
2023-04-17 21:40:06 +0530

    Add files via upload

---
+++ email.js
@@ -0,0 +1,21 @@
+console.log("Netflix is working!");
+
+function clicked() {
+    alert("Login successfully");
+    console.log("Successfully Login");
+}
+
+
+function LanguagesSelectValueTop() {
+    let selectValue = document.getElementById('TopPageSelectLanguages').value;
+    console.log("The current selected language is " + selectValue);
+    
+}
+
+
+function LanguagesSelectValueBottom() {
+    let selectValue = document.getElementById('BottomPageSelectLanguages').value;
+    console.log("The current selected language is " + selectValue);
+    
+}
+
